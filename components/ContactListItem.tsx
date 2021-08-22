a5d8a46f520828df9ffdcc83e814067e5b5160c5
DuyetVu2001 <vungocduyet24@gmail.com>
2021-08-23 00:54:36 +0700

    create chat room

--- components/ContactListItem.tsx
+++ components/ContactListItem.tsx
@@ -1,6 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
+import { API, Auth, graphqlOperation } from 'aws-amplify';
 import React from 'react';
 import { Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { createChatRoom, createChatRoomUser } from '../src/graphql/mutations';
 import { User } from '../types';
 import { Text, View } from './Themed';
 
@@ -12,7 +14,49 @@ export default function ContactListItem(props: ContactListItemProps) {
 	const { user } = props;
 	const navigation = useNavigation();
 
-	const handelPress = () => {};
+	const handelPress = async () => {
+		try {
+			// 1. Create a new chat room
+			const newChatRoomData: any = await API.graphql(
+				graphqlOperation(createChatRoom, { input: {} })
+			);
+
+			if (!newChatRoomData.data) {
+				console.log('Failed to create chat room!');
+				return;
+			}
+
+			const newChatRoom = newChatRoomData.data.createChatRoom;
+
+			// 2. Add 'user' to the chat room
+			await API.graphql(
+				graphqlOperation(createChatRoomUser, {
+					input: {
+						userID: user.id,
+						chatRoomID: newChatRoom.id,
+					},
+				})
+			);
+
+			// 3. Add authenticated user to the chat room
+			const userInfo = await Auth.currentAuthenticatedUser();
+			await API.graphql(
+				graphqlOperation(createChatRoomUser, {
+					input: {
+						userID: userInfo.attributes.sub,
+						chatRoomID: newChatRoom.id,
+					},
+				})
+			);
+
+			navigation.navigate('ChatRoom', {
+				id: newChatRoom.id,
+				name: 'Hardcoded',
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	return (
 		<TouchableOpacity activeOpacity={0.5} onPress={handelPress}>
